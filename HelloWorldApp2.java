c51c63d9b7b07e7cceefd8ded7aaaebae4b2cbd0
Gerald González Zeledón <gedgonz7@gmail.com>
2024-12-05 13:09:38 -0600

    documentando

--- HelloWorldApp2.java
+++ HelloWorldApp2.java
@@ -1,7 +1,11 @@
 public class  HelloWorldApp2{
     public static void main(String args[]){
 
-        String user = System.getenv("USERNAME");
+        //Se toma vairbale de entorno de github 
+        String user = System.getenv("USERNAME");       
+        String lenguaje = System.getenv("LANGUAGE");
         System.out.println("Hola "+user+" desde github");
+        System.out.println("Mi lenguaje favorito es: "+lenguaje);
+    }
     }
 }
