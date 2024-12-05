5fe758ef54c0a9c3639dd37a1704669968723b71
Gerald González Zeledón <gedgonz7@gmail.com>
2024-12-05 11:23:33 -0600

    nuevo action triggers

---
+++ HelloWorldApp2.java
@@ -0,0 +1,7 @@
+public class  HelloWorldApp2{
+    public static void main(String args[]){
+
+        String user = System.getenv("USERNAME");
+        System.out.println("Hola "+user+" desde github");
+    }
+}
