8001de5d52a325415654951ef29dbccdc7638244
Gerald González Zeledón <gedgonz7@gmail.com>
2024-12-05 12:57:50 -0600

    agregando nuevo workflow en main

---
+++ HolaMundo.java
@@ -0,0 +1,7 @@
+public class  HolaMundo{
+    public static void main(String args[]){
+
+        String user = System.getenv("USERNAME");
+        System.out.println("Hola "+user+" desde github");
+    }
+}
